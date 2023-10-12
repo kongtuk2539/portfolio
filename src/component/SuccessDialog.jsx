533dd3e7369f6929f075c081e8d741f8e3696f86
kongtuk2539 <thoty2539@gmail.com>
2023-10-12 08:39:25 +0700

    fix dialog

--- src/component/SuccessDialog.jsx
+++ src/component/SuccessDialog.jsx
@@ -12,7 +12,7 @@ function SuccessDialog({ message, onClose, duration }) {
     return (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-70 z-40">
             <div className="bg-[#F0B91E] w-[290px] lg:w-auto p-6 rounded shadow-lg text-center">
-                <p className="font-Lilita-One text-[20px] lg:text-[26px] text-[#D2272B]">{message}</p>
+                <p className="font-Lilita-One text-[20px] lg:text-[26px] text-[#000000]">Thank you <br /> {message}</p>
                 <button
                     onClick={onClose}
                     className="mt-4 bg-[#FFFFFF] hover:bg-[#FFFFFF]/[0.8] font-Lilita-One text-[18px] lg:text-[26px] text-[#D2272B] px-4 py-2 rounded"
